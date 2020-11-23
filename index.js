213bc2431322d3469f297c0eebe0d1d73d6d5549
Hanna <hanna.flores@unt.edu>
2020-11-22 20:29:43 -0600

    reset

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+/** @format */
+
 const express = require("express");
 const app = express();
 const http = require("http").Server(app);
+const fs = require("fs");
 
 app.get("/", function (req, res) {
-  res.sendFile("test.html", { root: __dirname });
+    res.sendFile("test.html", { root: __dirname });
+});
+
+app.get("/download", function (req, res) {
+    const file = `${__dirname}/test.bat`;
+    res.download(file); // Set disposition and send it.
 });
 
 const server = http.listen(process.env.PORT || 8080, function () {
-  console.log("listening on *:8080");
+    console.log("listening on *:8080");
 });
