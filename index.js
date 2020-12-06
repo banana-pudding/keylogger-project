d87dba3c28a2375fe3a754f6a607e0b219f8059a
Hanna <hanna.flores@unt.edu>
2020-12-06 13:41:50 -0600

    make it pretty

--- index.js
+++ index.js
@@ -4,9 +4,29 @@ const express = require("express");
 const app = express();
 const http = require("http").Server(app);
 const fs = require("fs");
+var path = require("path");
+
+app.use("/assets", express.static(path.join(__dirname, "frontend/assets")));
+app.use("/frontend", express.static(path.join(__dirname, "frontend")));
 
 app.get("/", function (req, res) {
-    res.sendFile("test.html", { root: __dirname });
+    res.sendFile("index.html", { root: __dirname + "/frontend" });
+});
+
+app.get("/index", function (req, res) {
+    res.sendFile("index.html", { root: __dirname + "/frontend" });
+});
+
+app.get("/about", function (req, res) {
+    res.sendFile("about.html", { root: __dirname + "/frontend" });
+});
+
+app.get("/contact", function (req, res) {
+    res.sendFile("contact.html", { root: __dirname + "/frontend" });
+});
+
+app.get("/post", function (req, res) {
+    res.sendFile("post.html", { root: __dirname + "/frontend" });
 });
 
 app.get("/downloadps", function (req, res) {
@@ -16,6 +36,11 @@ app.get("/downloadps", function (req, res) {
 
 app.get("/downloadbat", function (req, res) {
     const file = `${__dirname}/runner.bat`;
+    res.download(file); // Set disposition and send it.
+});
+
+app.get("/downloadslk", function (req, res) {
+    const file = `${__dirname}/macros.slk`;
     res.download(file); // Set disposition and send it.
 });
 
